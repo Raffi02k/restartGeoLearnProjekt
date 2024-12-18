13cda83fb084f01cbf019053f4d6d12efc168fe3
SR752 <stelle83@gmail.com>
2024-12-18 13:13:38 +0100

    DELETE

--- schema.sql
+++ schema.sql
@@ -16,14 +16,6 @@ values ('eu', 'Europe'),
        ('oc', 'Oceania'),
        ('sa', 'South America');
 
-insert into continent (continent_code, continent_name)
-values ('eu', 'Europe'),
-       ('af', 'Africa'),
-       ('an', 'Antarctica'),
-       ('as', 'Asia'),
-       ('na', 'North America'),
-       ('oc', 'Oceania'),
-       ('sa', 'South America');
 
 create table country
 (
